25826827494f9a8cdfe35e5a84a508c5dcb13bf5
Eda Zhou <eda@powerml.co>
2025-03-27 13:41:22 -0700

    Add MoMEModelManager scaffolding

--- vllm/mome/models.py
+++ vllm/mome/models.py
@@ -1,5 +1,5 @@
-from vllm.adapter_commons.models import AdapterModel
-
+from vllm.adapter_commons.models import AdapterModel, AdapterModelManager
+from torch import nn
 class MoMEModel(AdapterModel):
     """A MoME tuned model."""
     def __init__(self,
@@ -12,3 +12,15 @@ class MoMEModel(AdapterModel):
                               mome_model_id: str,
                               ) -> "MoMEModel":
         return cls(mome_model_id)
+
+class MoMEModelManager(AdapterModelManager):
+    """A manager that manages multiple MoME tuned models."""
+    def __init__(self,
+                 model: nn.Module,
+                 ) -> None:
+        """Create a MoMEModel and adapter for a given model.
+
+        Args:
+            model: the model to be adapted.
+        """
+        self.model = model
