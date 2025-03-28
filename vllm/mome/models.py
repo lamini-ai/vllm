0a5f8f5fd798ffd2a9f5029a458fc594fa114ee2
Eda Zhou <eda@powerml.co>
2025-03-28 12:18:33 -0700

    Add path, sentence transformer req

--- vllm/mome/models.py
+++ vllm/mome/models.py
@@ -1,26 +1,35 @@
 from vllm.adapter_commons.models import AdapterModel, AdapterModelManager
 from torch import nn
+from typing import Any, Dict, Optional
+from vllm.mome.model_definition.pretrained_lamini_mome_for_causal_lm import load_mome_model_for_inference
+    load_mome_model_for_inference
+
 class MoMEModel(AdapterModel):
     """A MoME tuned model."""
     def __init__(self,
                  mome_model_id: str,
+                 adapter_path: str,
                  ) -> None:
         """
         Args:
             mome_model_id: the id (model name) of the MoME model.
+            adapter_path: the path to the adapter weights.
         """
         self.id = mome_model_id
+        self.adapter_path = adapter_path
 
     @classmethod
     def from_local_checkpoint(cls,
                               mome_model_id: str,
+                              adapter_path: str,
                               ) -> "MoMEModel":
         """Create a MoMEModel from a local checkpoint.
 
         Args:
             mome_model_id: the id (model name) of the MoME model.
+            adapter_path: the path to the adapter weights.
         """
-        return cls(mome_model_id)
+        return cls(mome_model_id, adapter_path)
 
 class MoMEModelManager(AdapterModelManager):
     """A manager that manages multiple MoME tuned models."""
